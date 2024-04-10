c9f97b169f150a9cc4b36f820cf67e04e41fe273
0x41mujt4b4 <almujtaba007@gmail.com>
2024-04-09 19:03:11 -0700

    add 2- insert right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - insert node to the right of parent
+ *
+ * @parent: pointer to the parent node
+ * @value: the value of parent node
+ *
+ * Return: the node containing the value
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	if (!parent)
+		return (NULL);
+
+	binary_tree_t *node = binary_tree_node(parent, value);
+
+	if (!node)
+		return (NULL);
+	if (parent->right)
+	{
+		node->right = parent->right;
+		parent->right->parent = node;
+	}
+	parent->right = node;
+
+	return (node);
+}
