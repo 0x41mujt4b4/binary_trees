219270ea2ede2104ec16f674e9185adfad49e7ce
0x41mujt4b4 <almujtaba007@gmail.com>
2024-04-09 19:24:11 -0700

    add delete tree function

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - delete a binary tree
+ *
+ * @tree: the tree to be deleted
+ *
+ * Return: Nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		tree->n = NULL;
+		tree->left = NULL;
+		tree->right = NULL;
+	}
+}
