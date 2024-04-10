a3917cb11746fb7a254e4793961ad2ef81bed0db
0x41mujt4b4 <almujtaba007@gmail.com>
2024-04-09 20:20:12 -0700

    update isleaf function

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -5,11 +5,11 @@
  *
  * @node: the node to be checked
  *
- * Return 1 if node is a leaf 0 otherwise
+ * Return: 1 if node is a leaf 0 otherwise
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	return !(node->left || node->right);
+	return (!(node->left || node->right));
 }
