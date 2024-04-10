9fc185b0b50a769598ea7c6e7d09912d31295bf8
0x41mujt4b4 <almujtaba007@gmail.com>
2024-04-10 11:37:32 -0700

    update caclucate height function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,10 +9,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	int left_height;
+	int right_height;
+
 	if (!tree)
-		return (0);
-	int left_height = binary_tree_height(tree->left);
-	int right_height = binary_tree_height(tree->right);
+		return (-1);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return ((left_height > right_height ? left_height : right_height) + 1);
 }
