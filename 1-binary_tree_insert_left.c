fbc1b8d60d98e32fd04ff36d0479628210efa579
0x41mujt4b4 <almujtaba007@gmail.com>
2024-04-09 19:16:13 -0700

    update 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
-	binary_tree_t *node = binary_tree_node(parent, value);
+	binary_tree_t *node;
+	node = binary_tree_node(parent, value);
 
 	if (!node)
 		return (NULL);
