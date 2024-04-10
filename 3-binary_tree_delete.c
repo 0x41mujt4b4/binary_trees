a065914146233a8eff934812688d18dba7c76024
0x41mujt4b4 <almujtaba007@gmail.com>
2024-04-09 20:09:16 -0700

    fix delete function

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,23 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * free_bt - a function that frees a binary tree.
- * @tree: a pointer to the root node of the tree to be freed
- *
- * Return: Nothing
- */
-void free_bt(binary_tree_t *tree)
-{
-    if (tree == NULL)
-        return;
-
-    free_bt(tree->left);
-    free_bt(tree->right);
-
-    free(tree);
-}
-
-/**
  * binary_tree_delete - delete a binary tree
  *
  * @tree: the tree to be deleted
@@ -26,12 +9,10 @@ void free_bt(binary_tree_t *tree)
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (!tree)
-	{
-		free_bt(tree);
-		tree->left = NULL;
-		tree->right = NULL;
-		tree->n = 0;
-	}
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 
 }
--- binary_trees.h
+++ binary_trees.h
@@ -22,9 +22,6 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-
-void free_bt(binary_tree_t *tree);
-
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
