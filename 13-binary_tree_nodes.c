05c8f552cf92b10998074a2e20e7ee4ef49f9189
0x41mujt4b4 <almujtaba007@gmail.com>
2024-04-11 09:39:56 -0700

    add binary_tree_nodes function

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - return the size of a binary tree
+ *
+ * @tree: the tree to calculate it's size
+ *
+ * Return: the size of the tree otherwise 0
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	int left_nodes;
+	int right_nodes;
+
+	if (!tree || !tree->left || !tree->right)
+		return (0);
+	left_nodes = binary_tree_nodes(tree->left);
+	right_nodes = binary_tree_nodes(tree->right);
+
+	return (left_nodes + right_nodes + 1);
+}
