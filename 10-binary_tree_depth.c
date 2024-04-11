ed40ba8143238c852128227ea15792e835988d3d
0x41mujt4b4 <almujtaba007@gmail.com>
2024-04-11 08:07:27 -0700

    update depth function

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - return the height of a binary tree
+ * binary_tree_depth - return the depth of a binary tree
  *
  * @tree: the tree to calculate it's depth
  *
@@ -9,13 +9,7 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	int left_depth;
-	int right_depth;
-
-	if (!tree)
+	if (!tree || !tree->parent)
 		return (0);
-	left_depth = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right_depth = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-	return (left_depth > right_height ? left_height : right_height);
+	return (1 + binary_tree_depth(tree->parent));
 }
