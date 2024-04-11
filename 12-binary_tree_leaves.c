798985d10a9d3ae9f75bbdbcc4854487ec98b0f2
0x41mujt4b4 <almujtaba007@gmail.com>
2024-04-11 09:12:48 -0700

    update leaves function

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,7 +9,15 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	int left_leaves;
+	int right_leaves;
+
 	if (!tree)
 		return (0);
-	return ((tree->left && 1) + (tree->right && 1));
+	if (!tree->left && !tree->right)
+		return (1);
+	left_leaves = binary_tree_leaves(tree->left);
+	right_leaves = binary_tree_leaves(tree->right);
+
+	return (left_leaves + right_leaves);
 }
